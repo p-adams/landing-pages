8b400c94a653ef6bc1a55017bdff689547bd3b51
Paul Adams <ghusiyy@gmail.com>
2021-10-11 22:30:01 -0700

    remove boilerplate and add new assets and supporting component

---
+++ src/routes/firefox.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+  export const prerender = true;
+</script>
+
+<script lang="ts">
+  import Tile from "$lib/Tile.svelte";
+</script>
+
+<main>
+  <section>
+    <h1>Firefox</h1>
+
+    <Tile />
+  </section>
+</main>
